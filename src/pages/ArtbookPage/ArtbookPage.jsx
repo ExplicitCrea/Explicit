22133b7f29a3c0997477a0a3fbc02aee856857eb
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-01-17 00:41:28 +0100

    Creation of the Artbook page

---
+++ src/pages/ArtbookPage/ArtbookPage.jsx
@@ -0,0 +1,10 @@
+import { Artbook } from "../../components/Artbook/Artbook";
+import StyledArtbookPage from "./StyledArtbookPage";
+
+export const ArtbookPage = () => {
+  return (
+    <StyledArtbookPage>
+      <Artbook />
+    </StyledArtbookPage>
+  );
+};
---
+++ src/pages/ArtbookPage/StyledArtbookPage.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledArtbooksPage = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  color: white;
+  padding: 200px 50px 0px 50px;
+  background: linear-gradient(to right, #64ffa9, #634eff);
+`;
+
+export default StyledArtbooksPage;
