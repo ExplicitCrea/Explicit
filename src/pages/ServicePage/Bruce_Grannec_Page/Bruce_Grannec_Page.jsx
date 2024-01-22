fc7eb8b40fce8f2f6a426a3caf0b8f100d3b59b6
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-01-22 18:02:32 +0100

    Refacto class css

--- src/pages/ServicePage/Bruce_Grannec_Page/Bruce_Grannec_Page.jsx
+++ src/pages/ServicePage/Bruce_Grannec_Page/Bruce_Grannec_Page.jsx
@@ -1,59 +1,51 @@
-import { Carousel } from '../../../components/Bruce_Grannec/Carousel'
-import labelOnSide from '../../../assets/Bruce_Grannec/label_on_side.webp' 
-import whiteLogo from '../../../assets/Bruce_Grannec/white_logo.webp'
-import whiteStar from '../../../assets/Bruce_Grannec/white_star.webp'
-import eventPicture from '../../../assets/Bruce_Grannec/event_picture.webp'
-
-import {
-  StyledPage,
-  StyledLogo,
-  StyledStar,
-  StyledDivLabelOnSide,
-  StyledImgLabel,
-  StyledContainer,
-  StyledDivParagrah,
-  StyledDivLogo,
-  StyledParagraph,
-  StyledDivEventPicture,
-  StyledImageEventPicture,
-  StyledDivCarousel,
-}
-from './StyledBruceGrannec'
-
+import { Carousel } from "../../../components/Bruce_Grannec/Carousel";
+import labelOnSide from "../../../assets/Bruce_Grannec/label_on_side.webp";
+import whiteLogo from "../../../assets/Bruce_Grannec/white_logo.webp";
+import whiteStar from "../../../assets/Bruce_Grannec/white_star.webp";
+import eventPicture from "../../../assets/Bruce_Grannec/event_picture.webp";
+import { StyledPage } from "./StyledBruceGrannec";
 
 export const Bruce_Grannec_Page = () => {
   return (
     <StyledPage>
-      <StyledContainer>
-        
-        <StyledDivLabelOnSide>
-          <StyledImgLabel src={labelOnSide} alt='Label on side'/>
-        </StyledDivLabelOnSide>
-        
-        <StyledDivParagrah>
-          <StyledParagraph>
-            Nous sommes fiers d&apos;avoir pu collaborer avec Bruce Grannec en fournissant des visuels exceptionnels
-            pour son tournoi en collaboration avec FIFA 2023.
-            Notre équipe a conçu des assets animés sur mesure pour enrichir son événement. Chaque élément visuel a été soigneusement élaboré
-            pour refléter le professionnalisme et l&apos;enthousiasme du tournoi, offrant à Bruce Grannec
-            une présence visuelle distinctive et immersive sur sa plateforme de streaming.
-          </StyledParagraph>
-        </StyledDivParagrah>
+      <div className="container">
+        <div className="label-on-side">
+          <img src={labelOnSide} alt="Label on side" />
+        </div>
+        {/* <div className="paragraph-and-logo-container"> */}
+          <div className="paragraph_container">
+            <p>
+              Nous sommes fiers d&apos;avoir pu collaborer avec Bruce Grannec en
+              fournissant des visuels exceptionnels pour son tournoi en
+              collaboration avec FIFA 2023. Notre équipe a conçu des assets
+              animés sur mesure pour enrichir son événement. Chaque élément
+              visuel a été soigneusement élaboré pour refléter le
+              professionnalisme et l&apos;enthousiasme du tournoi, offrant à
+              Bruce Grannec une présence visuelle distinctive et immersive sur
+              sa plateforme de streaming.
+            </p>
+          </div>
+          <div className="logo-container">
+            <img
+              className="white-star"
+              src={whiteStar}
+              alt="White Logo Explicite"
+            />
+            <img
+              className="white-logo"
+              src={whiteLogo}
+              alt="White Star Explicite"
+            />
+          </div>
+        {/* </div> */}
+        <div className="event_container">
+          <img src={eventPicture} alt="Event Picture" />
+        </div>
 
-          <StyledDivLogo>
-            <StyledStar src={whiteStar} alt="White Logo Explicite" />
-            <StyledLogo src={whiteLogo} alt="White Star Explicite" />
-          </StyledDivLogo>
-
-        <StyledDivEventPicture>
-          <StyledImageEventPicture src={eventPicture} alt="Event Picture" />
-        </StyledDivEventPicture>
-        
-        <StyledDivCarousel>
-          <Carousel/>
-        </StyledDivCarousel>
-
-      </StyledContainer>
+        <div className="carousel-container">
+          <Carousel />
+        </div>
+      </div>
     </StyledPage>
-  )
-}
+  );
+};
--- src/pages/ServicePage/Bruce_Grannec_Page/StyledBruceGrannec.js
+++ src/pages/ServicePage/Bruce_Grannec_Page/StyledBruceGrannec.js
@@ -1,116 +1,96 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const StyledPage = styled.div`
-display: flex;
-background: url('src/assets/Bruce_Grannec/background.webp');
-background-size: cover;
-width: 100%;
-padding: 96px 15px 26px 15px;
-height: 100vh;
-border: 2px solid yellow;
-`
-const StyledContainer = styled.div`
-display: grid;
-justify-items: center;
-gap: 10px;
-margin: 0 auto;
-width: 100%;
-height: 100%;
-grid-template-columns: 1fr 2fr 3fr;
-grid-template-rows: 300px 100px 1fr;
-border: 2px solid red;
-`
+const StyledPage = styled.section`
+  display: flex;
+  background: url("src/assets/Bruce_Grannec/background.webp");
+  background-size: cover;
+  width: 100%;
+  padding: 96px 25px 26px 25px;
+  height: 100vh;
 
-// For the label on the left
+  .container {
+    display: grid;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    grid-template-columns: 15% 5% 10% 10% 30% 17%;
+    grid-template-rows: 40% 10% 20% 15% 15% 10% 5%;
+    border: 2px solid red;
+  }
 
-const StyledDivLabelOnSide = styled.div`
-grid-row: 1 / span 3;
-grid-column: 1;
-`
+  .label-on-side {
+    width: 100%;
+    height: 100%;
+    grid-row: 1 / span 5;
+    grid-column: 1;
 
-const StyledImgLabel = styled.img`
-width: 100%;
-height: 100%;
-object-fit: cover;
-`
-const StyledDivParagrah = styled.div`
-width: 100%;
-height: 100%;
-border: 1px solid pink;
-grid-row: 1;
-grid-column: 2;
-`
-const StyledParagraph = styled.p`
-border: 1px solid blue;
-width: 100%;
-height: 100%;
-margin: 0;
-font-family: Lexend;
-font-size: 1em;
-text-align: center;
-color: white;
-background-color: rgba(0, 0, 0, 0.7);
-padding: 2%;
-border-radius: 30px;
-`
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  
+  .paragraph_container {
+    width: 100%;
+    height: 100%;
+    grid-column: 3 / span 2;
+    grid-row: 1;
 
-const StyledDivLogo = styled.div`
-display: flex;
-grid-row: 1;
-align-items: center;
-justify-content: center;;
-gap: 5px;
-width: 30%;
-height: 100%;
-grid-row: 2;
-grid-column: 2;
-`
+    p {
+      margin: 0;
+      background-color: rgba(0, 0, 0, 0.7);
+      font-family: Lexend;
+      font-size: 0.9em;
+      text-align: center;
+      color: white;
+      padding: 10px;
+      border-radius: 30px;
+    }
+  }
 
-const StyledLogo = styled.img`
-display: inline;
-width: 150px;
-height: auto;
-object-fit: cover;
-`
-const StyledStar = styled.img`
-width: 90px;
-height: auto;
-object-fit: cover;
-`
+  .logo-container {
+    display: flex;
+    grid-row: 1;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    width: 100%;
+    height: 100%;
+    grid-row: 2;
+    grid-column: 3 / span 2;
 
-const StyledDivEventPicture = styled.div`
-width: 100%;
-height: 100%;
-grid-row: 1 / span 2;
-grid-column: 3 / span 2;
-` 
+    .white-star {
+      width: 70px;
+      height: auto;
+      object-fit: cover;
+    }
+    .white-logo {
+      display: inline;
+      width: 120px;
+      height: auto;
+      object-fit: cover;
+    }
+  }
 
-const StyledImageEventPicture = styled.img`
-object-fit: cover;
-width: 100%;
-height: 100%;
-`
+  .event_container {
+    height: 100%;
+    grid-column: 5 / span 2;
+    grid-row: 1 / span 2;
 
-const StyledDivCarousel = styled.div`
-width: 100%;
-height: 90%;
-margin-top: -2%;
-margin-left: -100%;
-grid-row: 3 / span 1;
-grid-column: 3 / span 1;
-`
- 
-export {
-    StyledPage,
-    StyledLogo, 
-    StyledStar,
-    StyledDivLabelOnSide,
-    StyledImgLabel,
-    StyledContainer,
-    StyledDivParagrah,
-    StyledDivLogo,
-    StyledParagraph,
-    StyledDivEventPicture,
-    StyledImageEventPicture,
-    StyledDivCarousel
-}
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .carousel-container {
+    width: 100%;
+    height: 100%;
+    grid-column: 3 / span 3;
+    grid-row: 3 / span 3;
+  }
+`;
+
+export { StyledPage };
