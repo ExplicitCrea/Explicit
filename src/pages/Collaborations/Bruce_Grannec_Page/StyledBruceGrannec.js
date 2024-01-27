dd53c21d4cde0f86efc20ae2918bd9518ea5ec55
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-01-27 16:10:15 +0100

    Modify css + add media query

--- src/pages/Collaborations/Bruce_Grannec_Page/StyledBruceGrannec.js
+++ src/pages/Collaborations/Bruce_Grannec_Page/StyledBruceGrannec.js
@@ -2,10 +2,12 @@ import styled from "styled-components";
 
 const StyledPage = styled.section`
   display: flex;
-  height: 100vh;
   background: url("src/assets/Bruce_Grannec/background.webp");
   background-size: cover;
-  padding: 1% 0%;
+  height: 100vh;
+  border: 2px solid yellow;
+  justify-content: center;
+  align-items: center;
 
   @media screen and (max-width: 1000px) {
     height: auto;
@@ -20,38 +22,44 @@ const StyledPage = styled.section`
 
   .container {
     display: grid;
-    justify-content: center;
     align-content: center;
     position: relative;
-    grid-column-gap: 4%;
-    grid-row-gap: 2%;
-    width: 100%;
-    height: 100%;
-    grid-template-columns: 14% 7% 10% 18% 7%;
-    grid-template-rows: 28% 8% 12% 26%;
+    grid-column-gap: 10px;
+    width: 90% ;
+    height: 80%;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 0.5fr 0.5fr 1fr 1fr;
+    border: 2px solid red;
 
     @media screen and (max-width: 1000px) {
       justify-items: center;
       grid-column-gap: 0%;
-      grid-row-gap: 2%;
-      padding: 96px 20px;
       grid-template-columns: 100%;
-      grid-template-rows: 25% auto 20% auto auto;
-
+      grid-template-rows: auto;
+      grid-row-gap: 10px;
+      padding: 96px 20px;
     }
 
     .label-on-side {
-      width: 100%;
-      height: auto;
+      width: 250px;
       aspect-ratio: 250 / 750;
       grid-row: 1 / span 3;
       grid-column: 1;
       gap: 10px;
+      border: 2px solid green;
 
       img {
         width: 100%;
-        height: 100%;
+        height: auto;
         object-fit: cover;
+      }
+      
+      @media screen and (max-width: 1600px) {
+        width: 220px;
+      }
+      
+      @media screen and (max-width: 1270px) {
+        width: 200px;
       }
 
       @media screen and (max-width: 1000px) {
@@ -68,7 +76,7 @@ const StyledPage = styled.section`
         height: 200px;
         min-height: 100px;
         max-height: 150px;
-        grid-row: 1;
+        grid-row: auto;
         grid-column: 1;
         border-radius: 5px;
       }
@@ -84,8 +92,6 @@ const StyledPage = styled.section`
           rgba(18, 25, 35, 1) 79%
         );
         box-shadow: 1px 3px 40px 10px rgba(0, 0, 0, 0.5);
-        
-
       }
 
       .banner-items {
@@ -94,7 +100,6 @@ const StyledPage = styled.section`
         align-items: center;
         width: 100%;
         gap: 5%;
-
       }
       .logo-fifa {
         width: 30%;
@@ -106,7 +111,6 @@ const StyledPage = styled.section`
         width: 25%;
         height: auto;
         object-fit: cover;
-
       }
 
       .twitch {
@@ -114,32 +118,29 @@ const StyledPage = styled.section`
         width: 30%;
         height: auto;
         object-fit: cover;
-
       }
     }
 
     .paragraph_container {
-      box-sizing: border-box;
-      width: 70%;
-      max-width: 70%;
-      height: auto;
       margin: 0 auto;
-      grid-column: 2 / span 2;
+      grid-column: 2;
+      width: 100%;
+
       grid-row: 1 / span 1;
+      border: 2px solid green;
 
       @media screen and (max-width: 1000px) {
         grid-column: 1;
-        grid-row: 4;
+        grid-row: auto;
         width: 100%;
-
       }
 
       p {
         margin: 0;
         background-color: rgba(0, 0, 0, 0.6);
         font-family: Lexend;
-        font-size: 0.8em;
-        padding: 8px;
+        font-size: 0.7em;
+        padding: 10px;
         text-align: center;
         color: white;
         width: 100%;
@@ -158,16 +159,18 @@ const StyledPage = styled.section`
     }
 
     .explicit-container {
-      margin-top: 2%;
       display: flex;
       align-items: center;
       justify-content: center;
+      margin: 0 auto;
+      position: relative;
+      top: 25px;
       gap: 3px;
-      width: 100%;
-      height: auto;
+      width: 15vw;
       grid-row: 2;
       grid-row: 2 / span 1;
-      grid-column: 2 / span 2;
+      grid-column: 2;
+      border: 2px solid green;
 
       @media screen and (max-width: 1000px) {
         width: 75vw;
@@ -190,33 +193,52 @@ const StyledPage = styled.section`
   }
 
   .event-container {
-    width: 100%;
-    height: 110%;
-    aspect-ratio: 4/3;
-    grid-column: 4 / span 2;
+    width: 600px;
+    height: auto;
+    /* height: 120%; */
+    aspect-ratio: 16/9;
+    grid-column: 3 / span 2;
     grid-row: 1 / span 2;
+    border: 2px solid green;
+
+    @media screen and (max-width: 1600px) {
+          font-size: 0.6em;
+        }
 
     img {
-      object-fit: cover;
       width: 100%;
       height: 100%;
+      object-fit: cover;
     }
-
-    @media screen and (max-width: 1000px) {
-      display: none;
-    }
+        @media screen and (max-width: 1270px) {
+          width: 530px;
+        }
+        @media screen and (max-width: 1210px) {
+          width: 470px;
+        }
+        @media screen and (max-width: 1000px) {
+          display: none;
+        }
   }
   .carousel-container {
     width: 100%;
-    aspect-ratio: 16/9;
+    position: relative;
+    top: 60px;
     height: auto;
-    grid-column: 2 / span 3;
-    grid-row: 3 / span 0;
+    aspect-ratio: 16/9;
+    grid-column: 2 / span 2;
+    grid-row: 3 / span 3;
+    border: 2px solid green;
+    @media screen and (max-width: 1270px) {
+      width: 80%;
+      grid-column: 2 / span 3;
+    }
+    @media screen and (max-width: 1210px) {
+      
+    }
     @media screen and (max-width: 1000px) {
-      aspect-ratio: 16/9;
       grid-column: 1;
       grid-row: 2;
-
     }
   }
 `;
