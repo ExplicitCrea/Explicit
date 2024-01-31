25b7b2bb75bcc4b49a742b18d9f3c0898f791008
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-01-31 12:50:31 +0100

    Set up logic for dynamic display of titles, paragraphs and video links

---
+++ src/components/Vzion/StyledDisplayContent.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const StyledDisplayContent = styled.div`
+
+  .text-content {
+      white-space: pre-line;
+    h3 {
+      margin: 0;
+      font-size: 43px;
+      line-height: 53.75px;
+      width: 655px;
+    }
+
+    button {
+      color: #fc0000;
+      font-size: 16px;
+      line-height: 20px;
+      cursor: pointer;
+      font-weight: bold;
+      border-radius: 7px;
+      padding: 7px;
+      background-color: #d9d9d900;
+      border: 1px solid #ffffff;
+    }
+  }
+`;
+
+export { StyledDisplayContent };
