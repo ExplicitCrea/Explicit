f96e7f35dcced8a1ced86c23cc61d51d6161e6f3
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-01 07:49:01 +0100

    Modify css

--- src/components/Vzion/CompareSlider.jsx/StyledCompareSlider.js
+++ src/components/Vzion/CompareSlider.jsx/StyledCompareSlider.js
@@ -3,25 +3,26 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
     width: 40%;
     @media (max-width: 1000px) {
-      width: 50%;
+      width: 70%;
+      .__rcs-handle-button {
+        width: 40px!important;
+        height: 40px!important;
+      }
     }
-    @media (max-width: 500px) {
-      width: 80% ;
-    }
-    
 
-  .react-compare-slider {
-    border-radius: 22px;
-    box-shadow: 4px 7px 15px 0px #0000007D;
-    @media (max-width: 1000px) {
-      border-radius: 0px ;
-    }
-    
-    
-    .react-compare-slider-image {
-    }
-  }
-
+      
+      .react-compare-slider {
+        border-radius: 22px;
+        box-shadow: 4px 7px 15px 0px #0000007D;
+        @media (max-width: 1000px) {
+          border-radius: 0px ;
+        }
+        
+        
+        .react-compare-slider-image {
+        }
+      }
+      
 `;
 
 export {StyledContainer}
--- src/components/Vzion/grid/StyledGrid.js
+++ src/components/Vzion/grid/StyledGrid.js
@@ -4,12 +4,11 @@ const StyledContainer = styled.div`
     
     display: flex;
     justify-content: center;
-    width: 100%;
     overflow: hidden;
-    border: 2px solid red;
-    @media (max-width: 1000px) {
-        padding: 0px 25px 0px 25px; ;
-    }
+    border: 2px solid blue;
+    
+
+   
     
 
     .grid {
@@ -18,14 +17,11 @@ const StyledContainer = styled.div`
         grid-template-columns: 33.33% 33.33% 33.33%;
         max-width: 781px;
         max-height: 502px;
+        border: 2px solid blue;
         img {
             width: 100%;
             height: 100%;
             object-fit: cover;
-            @media (max-width: 745px) {
-                width: 154.82px;
-                height: 147.19px; ;
-            }
         }
     }
 `
