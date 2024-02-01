1681d1fe40de9fb5b2d58e4e860975ce2a5c24a4
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-01 06:53:59 +0100

    Responsive upgrade

--- src/components/Vzion/CompareSlider.jsx/StyledCompareSlider.js
+++ src/components/Vzion/CompareSlider.jsx/StyledCompareSlider.js
@@ -1,15 +1,21 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-    width: 35%;
+    width: 40%;
     @media (max-width: 1000px) {
       width: 50%;
+    }
+    @media (max-width: 500px) {
+      width: 80% ;
     }
     
 
   .react-compare-slider {
     border-radius: 22px;
     box-shadow: 4px 7px 15px 0px #0000007D;
+    @media (max-width: 1000px) {
+      border-radius: 0px ;
+    }
     
     
     .react-compare-slider-image {
--- src/components/Vzion/grid/StyledGrid.js
+++ src/components/Vzion/grid/StyledGrid.js
@@ -5,22 +5,27 @@ const StyledContainer = styled.div`
     display: flex;
     justify-content: center;
     width: 100%;
-    margin-bottom: 50px;
     overflow: hidden;
+    border: 2px solid red;
+    @media (max-width: 1000px) {
+        padding: 0px 25px 0px 25px; ;
+    }
     
 
     .grid {
         display: grid;
         grid-template-rows: 50% 50%;
         grid-template-columns: 33.33% 33.33% 33.33%;
-        width: 100%;
         max-width: 781px;
         max-height: 502px;
-
         img {
             width: 100%;
             height: 100%;
             object-fit: cover;
+            @media (max-width: 745px) {
+                width: 154.82px;
+                height: 147.19px; ;
+            }
         }
     }
 `
