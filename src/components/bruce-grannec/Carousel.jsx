822258506ad3300b9deaf8f58bbc68cc73058971
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-07 22:25:23 +0100

    Adding a carousel with + dotes

--- src/components/bruce-grannec/Carousel.jsx
+++ src/components/bruce-grannec/Carousel.jsx
@@ -2,33 +2,81 @@ import { useState, useEffect } from "react";
 import diapo1 from "../../assets/bruce-grannec/carousel/diapo1.webp";
 import diapo2 from "../../assets/bruce-grannec/carousel/diapo2.webp";
 import diapo3 from "../../assets/bruce-grannec/carousel/diapo3.webp";
-import { CarouselImg } from "./StyledCarousel";
+import { StyledCarousel } from "./StyledCarousel";
 
 export const Carousel = () => {
   const diapos = [diapo1, diapo2, diapo3];
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      changeCurrentIndex();
-    }, 15000);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [currentIndex]);
-
-  const changeCurrentIndex = () => {
-    if (currentIndex === diapos.length - 1) {
-      setCurrentIndex(0);
-    } else {
-      setCurrentIndex(currentIndex + 1);
+  const prevDiapo = () => {
+    if(currentIndex === 0) {
+      setCurrentIndex(diapos.length-1)
+    }else {
+      setCurrentIndex(currentIndex-1)
     }
-  };
+  }
+
+  const afterDiapo = () => {
+    if(currentIndex === diapos.length-1){
+      setCurrentIndex(0)
+    } else {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
 
   return (
-    <>
-      <CarouselImg src={diapos[currentIndex]} alt={`image ${currentIndex}`} />
-    </>
+    <StyledCarousel>
+      <div className="container-arrow-left">
+        <svg
+          onClick={prevDiapo}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="arrow arrow-left w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </div>
+      <img
+        className="carousel-img"
+        src={diapos[currentIndex]}
+        alt={`image ${currentIndex}`}
+      />
+      <div className="container-arrow-right">
+        <svg
+          onClick={afterDiapo}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="arrow arrow-right w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </div>
+      <div className="radio-btn">
+        {diapos.map((index, diapo) => (
+          <input
+            key={index}
+            type="radio"
+            name="radio-button"
+            checked={currentIndex === diapo}
+            onChange={() => setCurrentIndex(diapo)}
+            />
+        ))}
+      </div>
+    </StyledCarousel>
   );
 };
--- src/components/bruce-grannec/StyledCarousel.js
+++ src/components/bruce-grannec/StyledCarousel.js
@@ -1,10 +1,74 @@
 import styled from "styled-components";
 
-const CarouselImg = styled.img`
+const StyledCarousel = styled.div`
+  position: relative;
+
+  img {
+    -khtml-user-select: none;
+    -o-user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+  }
+
+  .arrow {
+    position: absolute;
+    width: 50px;
+    height: auto;
+    color: white;
+    cursor: pointer;
+  }
+
+  .container-arrow-left {
+    width: 8%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+    .arrow-left {
+      top: 50%;
+      right: 50%;
+      transform: translate(50%, -50%);
+    }
+  }
+
+  .container-arrow-right {
+    width: 8%;
+    height: 100%;
+    position: absolute;
+    right: 0;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+
+    .arrow-right {
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+
+  .radio-btn {
+    position: absolute;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    width: 100%;
+    height: 10%;
+    border-radius: 10px;
+    input {
+      cursor: pointer;
+      background-color: red;
+    }
+  }
+
+  .carousel-img {
     width: 100%;
     height: auto;
     object-fit: cover;
-    box-shadow: 0px 4px 30px 14px rgba(0,0,0,0.2);
+  }
 `;
 
-export { CarouselImg };
+export { StyledCarousel };
