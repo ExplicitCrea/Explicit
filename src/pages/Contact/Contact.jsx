fb95752c6f670b0200d5aad8bad5beca2a645e25
Devtrotter <sebastienchapuy19@gmail.com>
2024-02-13 12:14:00 +0100

    fix correction of contact page + add background général

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import StyledContact from './StyledContact'
 import {SendEmail}  from '../../utils/E-mail/SendEmail';
 import { ToastContainer, toast } from 'react-toastify';
@@ -12,68 +12,64 @@ export const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (form.current.checkValidity()) {
-            SendEmail(form.current)
-                .then((response) => {
-                    console.log('Email sent successfully:', response);
-                    form.current.reset();
-                    notifySuccess();
-                })
-                .catch((error) => {
-                    console.error('Failed to send email:', error);
-                    alert('Erreur lors de l\'envoi de l\'e-mail. Veuillez réessayer.');
-                    alert('Veuillez remplir tous les champs du formulaire.');
-                });
-        } else {
-        }
+        SendEmail(form.current)
+            .then((response) => {
+                console.log('Email sent successfully:', response);
+                form.current.reset();
+                notifySuccess();
+            })
+            .catch((error) => {
+                console.error('Failed to send email:', error);
+                alert('Erreur lors de l\'envoi de l\'e-mail. Veuillez réessayer.');
+                alert('Veuillez remplir tous les champs du formulaire.');
+            });
     };
 
 return (
     <StyledContact>
-            <h1>Nous Contacter</h1>
-    <form ref={form} onSubmit={handleSubmit}>
-        <div>
-            <label type="text">Votre Prénom / Nom</label>
-        </div>
-        <div className='container'>
-            <input type='text' name='prenom'  placeholder='Prénom' required/>
-            <input type='text' name='nom'     placeholder='Nom' required/>
-        </div>
-            <label type="mail"> Adresse E-Mail*</label>
-        <div>
-            <input type='email' name='email'  placeholder='exemple@exemple.com' required/>
-        </div>
-            <label type="phone">Numéro de Téléphone</label>
-        <div>
-            <input type='phone' name='phone'  placeholder='+33 01 23 45 67 89' required/>
-        </div>
-            <label type="entreprise">Nom d'entreprise/Pseudonyme*</label>
-        <div>
-            <input type='text' name='entreprise'  placeholder=' Ex.nom de l entreprise/Pseudo' required /> 
-        </div>
-            <label type="message">Message*</label>
-        <div>
-                <textarea type='text' name='message' placeholder='Ajouter un message' required/>
-        </div>
-            <button  type='submit' className="send-button">Envoyer</button>
-            <input type="hidden" name="a_password"  tabindex="-1" autocomplete="off"/>
-    <ToastContainer />
-        <div className='coordonner'>
+        <h1>Nous Contacter</h1>
+        <form ref={form} onSubmit={handleSubmit}>
+            <div className='contact'>
                 <h2>Pour nous contacter</h2>
-                    <li>
-                        <a href="mailto:Contact@explicitcrea.com">
-                            <MdEmail /> Contact@explicitcrea.com
-                        </a>
-                    </li>
-                    <li>
-                        <a href="tel:07.83.82.05.02">
-                            <BsFillTelephoneFill />  07.83.82.05.02
-                        </a>
-                    </li>
-                    
-        </div>
-    </form>
-        <div className='background-filter'/>
+                <li>
+                    <a href="mailto:Contact@explicitcrea.com">
+                        <MdEmail /> Contact@explicitcrea.com
+                    </a>
+                </li>
+                <li>
+                    <a href="tel:07.83.82.05.02">
+                        <BsFillTelephoneFill />  07.83.82.05.02
+                    </a>
+                </li>
+            </div>
+            <div>
+                <label type="text">Votre Prénom / Nom</label>
+            </div>
+            <div className='container'>
+                <input type='text' name='prenom'  placeholder='Prénom' required/>
+                <input type='text' name='nom'     placeholder='Nom' required/>
+            </div>
+                <label type="mail"> Adresse E-Mail*</label>
+            <div>
+                <input type='email' name='email'  placeholder='exemple@exemple.com' required/>
+            </div>
+                <label type="phone">Numéro de Téléphone</label>
+            <div>
+                <input type='phone' name='phone'  placeholder='+33 01 23 45 67 89' required/>
+            </div>
+                <label type="entreprise">Nom d'entreprise/Pseudonyme*</label>
+            <div>
+                <input type='text' name='entreprise'  placeholder=' Ex.nom de l entreprise/Pseudo' required /> 
+            </div>
+                <label type="message">Message*</label>
+            <div>
+                    <textarea type='text' name='message' placeholder='Ajouter un message' required/>
+            </div>
+                <button  type='submit' className="send-button">Envoyer</button>
+                <input type="hidden" name="a_password"  tabIndex="-1" autoComplete="off"/>
+            <ToastContainer />
+        </form>
+        <div className='linear-background'/>
     </StyledContact>
 
 )
--- src/pages/Contact/StyledContact.js
+++ src/pages/Contact/StyledContact.js
@@ -1,15 +1,31 @@
 import styled from 'styled-components'
 
-const StyledContact = styled.div`
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
+const StyledContact = styled.section`
 width: 100%;
-height: 100%; 
+min-height: 100vh;
 padding: 100px; 
 background-color: #191C2E;
-position: fixed;
-overflow: hidden;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+.linear-background{
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(39deg, rgb(100 255 133 / 26%) 1.89%, rgb(40 32 143 / 45%) 81.57%);
+    z-index: 0;
+}
+
+h1{
+    color: white;
+    font-size:40px ;
+    font-family: lexend;
+    text-align: center;
+    z-index: 1;
+}
 
 form{
     display: flex;
@@ -17,45 +33,42 @@ form{
     position: relative;
     width: 35%;
     padding: 6px;
-    margin: 28px auto;
-}
-    
-    h1{
-    color: white;
-    font-size:40px ;
-    font-family: lexend;
-    text-align: center;
-    }
+    margin: 28px 0 0 0;
+    z-index: 1;
+
     label{
         color: white;
         font-size:15px ;
         font-family: lexend;
     }
+
     input, textarea{
-    display: flex;
-    flex: 1;
-    margin-right: 35px; 
-    width: 94%;
-    height: 100%;
-    margin-bottom: 20px;
-    border: 0.8px solid rgba(255, 255, 255, 0.1);
-    border-radius: 25px;
-    background-color: transparent;
-    padding: 12px;
-    outline: none;
-    text-align: left;
-    color: white;
+        display: flex;
+        flex: 1;
+        margin-right: 35px; 
+        width: 94%;
+        height: 100%;
+        margin-bottom: 20px;
+        border: 0.8px solid rgba(255, 255, 255, 0.1);
+        border-radius: 25px;
+        background-color: transparent;
+        padding: 12px;
+        outline: none;
+        text-align: left;
+        color: white;
     }
+
     textarea{
-    height: 130px;
+        height: 130px;
     
     }
-.container{
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
+
+    .container{
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
     }
-    
+
     .send-button {
         display: flex;
         background-color: #2f2e2e;
@@ -69,93 +82,106 @@ form{
         font-size: 15px;
         margin: 40px auto;
     }
-    .Toastify__toast-container {
-            padding: 80px 50px 0px 30px;
-}
-.coordonner {
-    position: fixed;
-    display: flex;
-    flex-direction: column;
-    border: 0.8px solid rgba(255, 255, 255, 0.1);
-    border-radius: 12px;
-    background-color: transparent;
-    width: 18vw;
-    font-size: 14px;
-    padding: 0px;
-    height: auto;
-    margin: 100px;
-    right: 103px;
-    text-align: center;
-    color: white;
-    font-family: lexend;
-    
+
+    .contact {
+        position: absolute;
+        top: 24px;
+        right: -260px;
+        display: flex;
+        flex-direction: column;
+        border: 0.8px solid rgba(255, 255, 255, 0.1);
+        border-radius: 12px;
+        background-color: transparent;
+        width: 260px;
+        font-size: 14px;
+        padding: 0px;
+        height: auto;
+        color: white;
+        font-family: lexend;
+
+        h2{
+            text-align: center;
+        }
+        
         li{
             flex-wrap: 1;
             margin-bottom: 14px;
-    }
-            a{
-                text-decoration: none;
-                color: white;
+            list-style: none;
         }
-}
-    .background-filter {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: linear-gradient(39deg, rgb(100 255 133 / 26%) 1.89%, rgb(40 32 143 / 45%) 81.57%);
-        z-index: -1;
-    }
-
-    @media screen and (max-width: 1000px) {
-        form {
+        a{
             display: flex;
-            flex-direction: column;
+            align-items: center;
+            text-decoration: none;
+            color: white;
+
+            svg{
+                width: 20px;
+                height: 20px;
+                margin: 0 12px;
+            }
+        }
+    }
+}
+
+.Toastify__toast-container {
+    padding: 80px 50px 0px 30px;
+}
+
+
+@media screen and (max-width: 1000px) {
+    padding: 92px 0 0 0;
+    form {
+        display: flex;
+        flex-direction: column;
+        width: 80%;
+        padding: 0px;
+        margin: 0;
+
+        .contact {
+            position: unset;
             width: 100%;
-            padding: 0px;
-            margin: 30px auto;
+            li{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+            margin: 0 0 12px 0;
         }
-        .container{
-            display: flex;
-            flex-direction: column;
-        
-        }
-        h1 {
-            font-size: 28px;
-            margin: 0px 0px 31px 0px;
-        }
-        label {
-            font-size:9px ;
-        }
-        input, textarea {
-            margin-right: 6px;
-            width: 98%;
-            height: 100%;
-            margin-bottom: 16px;
-            padding: 5px;
-        textarea{
-            height: 130px;
-    }
-}
-.coordonner {
-    width: 45%;
-    left: 23%;
-    transform: translate(-50%, -50%);
-    margin-top: -15px;
-    height: 43px;
-    list-style: none;
-    h2{
-    display: none;
-    }
-    a{
-    font-size: 10px;
-    }
-    li{
-        margin-bottom: 0px 
-    }
-}
 
+        input, textarea {
+            width: 100%;
+            margin-right: 0;
+            margin-bottom: 16px;
+            border-radius: 12px;
+
+            textarea{
+                height: 130px;
+                padding: 12px 0 0 12px;
+            }
+        }
+
+        input{
+            height: 48px;
+            padding: 0 0 0 12px;
+        }
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        height: 124px;
+
+        input{
+            height: 48px;
+            padding: 0 0 0 12px;
+        }
+    }
+    h1 {
+        font-size: 28px;
+        margin: 0px 0px 31px 0px;
+    }
+    label {
+        font-size:9px ;
+    }
 }
 
 
