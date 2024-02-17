d1078e1cc6ad300e469d2738a13decc1daac8900
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-17 15:22:43 +0100

    Modify css

--- src/pages/Wrldmag/Wrldmag.jsx
+++ src/pages/Wrldmag/Wrldmag.jsx
@@ -2,7 +2,7 @@ import logo from "../../assets/wrldmag/logo.png";
 import image1 from "../../assets/wrldmag/image1.png";
 import image2 from "../../assets/wrldmag/image2.png";
 import image3 from "../../assets/wrldmag/image3.png";
-import wrldmag from "../../assets/wrldmag/wrldmag.png";
+import wrldmagLarge from "../../assets/wrldmag/wrldmag.png";
 import bgBottom from "../../assets/wrldmag/bg-bottom.png";
 import { useTranslation } from "react-i18next";
 import { StyledWrldmag } from "./StyledWrldmag";
@@ -16,9 +16,7 @@ export const Wrldmag = () => {
       <div className="container">
         <div className="main-wrapper">
           <img src={logo} className="logo" alt="" />
-          <p>
-            {t('wrldmag.paragraph')}
-          </p>
+          <p>{t("wrldmag.paragraph")}</p>
         </div>
         <div className="second-wrapper">
           <div className="images-wrapper">
@@ -26,14 +24,14 @@ export const Wrldmag = () => {
             <img src={image1} className="image-center" alt="" />
             <img src={image3} className="image-right" alt="" />
           </div>
-        <div className="wrldmag">
-          <img src={wrldmag} alt="" />
+            <div className="wrldmag-container">
+              <img className="wrldmag-large" src={wrldmagLarge} alt="" />
+            </div>
+            <div className="bg-bottom">
+              <img src={bgBottom} alt="" />
+            </div>
+          </div>
         </div>
-        <div className="bg-bottom">
-          <img src={bgBottom} alt="" />
-        </div>
-        </div>
-      </div>
     </StyledWrldmag>
   );
 };
