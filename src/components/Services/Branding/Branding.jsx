0a391c2e93ffcb192cec852b85cce17dde359470
Devtrotter <sebastienchapuy19@gmail.com>
2024-02-18 12:52:10 +0100

    add section branding and graphics animation

--- src/components/Services/Branding/Branding.jsx
+++ src/components/Services/Branding/Branding.jsx
@@ -9,11 +9,44 @@ import display7 from "../../../assets/services/branding/display7.png"
 import display8 from "../../../assets/services/branding/display8.png"
 import logo from "../../../assets/services/branding/logo.png"
 import logo2 from "../../../assets/services/branding/logo2.png"
+import { useState, useCallback } from 'react';
+import { throttle } from '../../../utils/throttle';
 
 
 export const Branding = () => {
+  const [rotate, setRotate] = useState({x: -0.71, y: 27.5});
+
+  const onMouseMove = useCallback(throttle((e) => {
+      const card = e.currentTarget;
+      const box = card.getBoundingClientRect();
+      const x = e.clientX - box.left;
+      const y = e.clientY - box.top;
+      const centerX = box.width / 2;
+      const centerY = box.height / 2;
+      const rotateX = (y - centerY) / 11;
+      const rotateY = (centerX - x) / 11;
+
+      setRotate({x: rotateX, y: rotateY});
+    }, 100),
+    []
+  )
+
+  const onMouseLeave = () => {
+    setRotate({x: -0.71, y: 27.5});
+  }
+
+  const stylePosterLeft = {
+    transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+    transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+  }
+
+  const stylePosterRight = {
+    transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${-rotate.y}deg) scale3d(1, 1, 1)`,
+    transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+  }
+
   return (
-    <StyledBranding>
+    <StyledBranding onMouseMove={onMouseMove} onMouseLeave={onMouseLeave}>
       <div className='text-container'>
         <h1>Branding</h1>
         <p>
@@ -22,17 +55,17 @@ export const Branding = () => {
       </div>
       <div className="poster-container">
         <div className="little-poster">
-          <img src={display3} alt="wrldmag poster"/>
-          <img src={display4} alt="wrldmag poster"/>
-          <img src={display6} alt="wrldmag poster"/>
+          <img style={stylePosterLeft} src={display3} alt="wrldmag poster"/>
+          <img style={stylePosterLeft} src={display4} alt="wrldmag poster"/>
+          <img style={stylePosterLeft} src={display6} alt="wrldmag poster"/>
         </div>
-        <img className="big-poster" src={display5} alt="wrldmag poster"/>
+        <img style={stylePosterLeft} className="big-poster" src={display5} alt="wrldmag poster"/>
         <img className="logo" src={logo} alt="wrldmag logo"/>
-        <img className="big-poster" src={display2} alt="wrldmag poster"/>
+        <img style={stylePosterRight} className="big-poster" src={display2} alt="wrldmag poster"/>
         <div className="little-poster">
-          <img src={display1} alt="wrldmag poster"/>
-          <img src={display7} alt="wrldmag poster"/>
-          <img src={display8} alt="wrldmag poster"/>
+          <img style={stylePosterRight} src={display1} alt="wrldmag poster"/>
+          <img style={stylePosterRight} src={display7} alt="wrldmag poster"/>
+          <img style={stylePosterRight} src={display8} alt="wrldmag poster"/>
         </div>
       </div>
       <img className="second-logo" src={logo2} alt="wrldmag logo"/>
